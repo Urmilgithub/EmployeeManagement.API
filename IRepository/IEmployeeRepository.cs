36e03d989144f7742617043c5cfdaa5f20f1fd6d
Urmilgithub <urmilpatelit27@gmail.com>
2026-03-29 21:18:05 +0530

    Feat: Pagination added in employee repository

--- IRepository/IEmployeeRepository.cs
+++ IRepository/IEmployeeRepository.cs
@@ -5,10 +5,10 @@ namespace EmployeeManagement.IRepository
 {
     public interface IEmployeeRepository
     {
-        public Task<IEnumerable<EmployeeDTO>> GetEmployeeListAsync(string? name, string? state,
+        public Task<PaginatedResultDTO<EmployeeDTO>> GetEmployeeListAsync(string? name, string? state,
                                                                    string? department, string? job,
                                                                    string? city, string? sortOrder, 
-                                                                   string? sortBy);
+                                                                   string? sortBy, int page = 1);
         public Task<EmployeeDTO?> GetEmployeeByIdAsync(Int64 id);
         public Task<AddEmployeeDTO> AddEmployeeAsync(AddEmployeeDTO addEmployeeDTO);
         public Task<UpdateEmployeeDTO?> UpdateEmployeeByIdAsync(Int64 id, UpdateEmployeeDTO updateEmployeeDTO);
---
+++ Model/DTO/PaginatedResultDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagement.Model.DTO
+{
+    public class PaginatedResultDTO<T>
+    {
+        public IEnumerable<T> Items { get; set; } = [];
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int PageNumber {  get; set; }
+        public int PageSize { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextpage => PageNumber < TotalPages;
+    }
+}
