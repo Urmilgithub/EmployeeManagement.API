2e93a2c6579434dfe726bd3bd45bf3c55c76ffd6
Urmilgithub <urmilpatelit27@gmail.com>
2026-04-02 23:58:13 +0530

    Refractor: code refractor in employee list method in employee repository

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -10,7 +10,7 @@ namespace EmployeeManagement.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeRepository employeeRepository;
