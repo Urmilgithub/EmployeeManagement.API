e82344538c2427d4d4ec449e2be24f966a801ee1
Urmilgithub <urmilpatelit27@gmail.com>
2026-04-09 22:55:18 +0530

    fix: StateName created nullable type in citydto

--- Model/DTO/DepartmentDTO.cs
+++ Model/DTO/DepartmentDTO.cs
@@ -7,6 +7,6 @@ namespace EmployeeManagement.Model.DTO
         public long DepartmentId { get; set; }
         public string? DepartmentName { get; set; }
         public long? CityId { get; set; }
-        public string CityName {  get; set; } 
+        public string? CityName {  get; set; } 
     }
 }
